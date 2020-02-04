fc1374a27992b760b8fa9e5d6d565164a55d38ff
wangchen <1147607253@qq.com>
2020-02-04 20:30:11 +0800

    project init

---
+++ springbootDemo/src/main/java/com/enjoy/wcy/springbootDemo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.enjoy.wcy.springbootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
